e41ed806c1b9eeb9302e184384acf7616c19f11e
blue <blue.4e2@gmail.com>
2024-12-08 11:56:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://purewear.vercel.app',
+    methods: ['GET', 'POST', 'DELETE','PUT'],
     credentials: true
 }));
 app.use(express.json());
